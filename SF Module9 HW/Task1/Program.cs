a85ee9f3685d87bab2f0f315bce0846b268792f7
Dmitry <ryabchikk@rambler.ru>
2022-11-01 19:01:04 +0300

    task1

--- SF Module9 HW/Task1/Program.cs
+++ SF Module9 HW/Task1/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            if (true == false) throw new ExceptionExtension("Моё исключение");
+        }
+        catch (ExceptionExtension ex)
+        {
+            Console.WriteLine("Системное описание :", ex.Message);
+        }
+        catch (System.InvalidCastException ex)
+        {
+            Console.WriteLine("Невозможно произвести преобразование!");
+            Console.WriteLine("Системное описание :", ex.Message);
+        }
+        catch (System.NullReferenceException ex)
+        {
+            Console.WriteLine("Пустая ссылка!");
+            Console.WriteLine("Системное описание :", ex.Message);
+        }
+        catch (System.DivideByZeroException ex)
+        {
+            Console.WriteLine("На ноль делить нельзя!");
+            Console.WriteLine("Системное описание :", ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Произошла непредвиденная ошибка в приложении.");
+            Console.WriteLine("Системное описание :", ex.Message);
+        }
+        finally
+        {
+            //Console.WriteLine("Блок Finally");
+        }
+    }
+}
+
+class ExceptionExtension : Exception
+{
+    public ExceptionExtension()
+    { }
+    public ExceptionExtension(string name) : base(name)
+    { }
+}
--- SF Module9 HW/Task2/Program.cs
+++ SF Module9 HW/Task2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+
+    public delegate void SortDelegate();
+    public event SortDelegate SortEvent;
+    private static void Main(string[] args)
+    {
+        SortDelegate sortDelegate;
+        
+
+        Console.WriteLine("Hello, World!");
+    }
+
+}
